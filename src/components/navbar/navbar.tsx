22badb209f742e84626b5ac105fbf1eff106a9b5
신승혁 <Cozak92@gmail.com>
2022-01-20 23:05:35 +0900

    ♻️navbar 코드 변경

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,21 +1,33 @@
 import React from 'react';
-import {  Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 
-export default function Navbar(){
+export default function Navbar() {
     return (
         <div>
-            <li>
-              <Link to="/">Dogs</Link>
-            </li>
-            <li>
-              <Link to="/cats">Cats</Link>
-            </li>
-            <li>
-              <Link to="/sheeps">Sheeps</Link>
-            </li>
-            <li>
-              <Link to="/goats">Goats</Link>
-            </li>
-          </div>
+            <Nav>
+                <NavLink to="/">
+                    <h1>Logo</h1>
+                </NavLink>
+                <Bars/>
+                <NavMenu>
+                    <NavLink to="/dev" activeStyle>
+                        데브매칭
+                    </NavLink>
+                    <NavLink to="/community" activeStyle>
+                        커뮤니티
+                    </NavLink>
+                    <NavLink to="/study" activeStyle>
+                        스터디
+                    </NavLink>
+                </NavMenu>
+                <Bars />
+                <NavLink to="/profile">
+                    <h1>프로필</h1>
+                </NavLink>
+                <NavLink to="/settings">
+                    <h1>설정</h1>
+                </NavLink>
+            </Nav>
+        </div>
     );
 }
